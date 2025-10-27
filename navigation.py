a5cd65faca5860b7d803cabe947b9811d32f8a4a
AadityaThakkar21 <thakkaraaditya21@gmail.com>
2025-10-28 00:04:11 +0530

    minor changes

--- navigation.py
+++ navigation.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 # Define the pages
-main_page = st.Page("main.py", title="Main Page", icon="🎈")
-TimeTable = st.Page("Time_table.py", title="Time Table", icon="❄️")
-QuizMaker = st.Page("quiz_maker.py", title="Quiz Maker", icon="🎉")
-Leaderboard = st.Page("leaderboard.py", title="Leaderboard", icon="🎉")
+main_page = st.Page("main.py", title="Home", icon="🏠")
+TimeTable = st.Page("Time_table.py", title="Time Table", icon="📅")
+QuizMaker = st.Page("quiz_maker.py", title="Quiz Maker", icon="📝")
+Leaderboard = st.Page("leaderboard.py", title="Leaderboard", icon="🏆")
+StudyGroup = st.Page("study_group.py", title="Study Group", icon="👥")
 # CourseSuggestor = st.Page("course_suggestor.py", title="Course Suggestor", icon="🎉")
 
 # Set up navigation
