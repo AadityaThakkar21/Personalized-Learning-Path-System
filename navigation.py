d85ac35708b813e52dfa31d59b84c5218fdbc4ff
AadityaThakkar21 <thakkaraaditya21@gmail.com>
2025-10-28 00:08:02 +0530

    added studygroup

--- navigation.py
+++ navigation.py
@@ -6,10 +6,9 @@ TimeTable = st.Page("Time_table.py", title="Time Table", icon="📅")
 QuizMaker = st.Page("quiz_maker.py", title="Quiz Maker", icon="📝")
 Leaderboard = st.Page("leaderboard.py", title="Leaderboard", icon="🏆")
 StudyGroup = st.Page("study_group.py", title="Study Group", icon="👥")
-# CourseSuggestor = st.Page("course_suggestor.py", title="Course Suggestor", icon="🎉")
 
 # Set up navigation
-pg = st.navigation([main_page, TimeTable, QuizMaker, Leaderboard])
+pg = st.navigation([main_page, TimeTable, QuizMaker, Leaderboard, StudyGroup])
 
 # Run the selected page
 pg.run()
