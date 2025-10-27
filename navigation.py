d93cb8dd3e0aa5260239b32946cfba6c2e4325a0
AadityaThakkar21 <thakkaraaditya21@gmail.com>
2025-10-27 11:32:01 +0530

    create separate pages using streamlit

---
+++ navigation.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Define the pages
+main_page = st.Page("main.py", title="Main Page", icon="🎈")
+TimeTable = st.Page("Time_table.py", title="Time Table", icon="❄️")
+QuizMaker = st.Page("quiz_maker.py", title="Quiz Maker", icon="🎉")
+Leaderboard = st.Page("leaderboard.py", title="Leaderboard", icon="🎉")
+# CourseSuggestor = st.Page("course_suggestor.py", title="Course Suggestor", icon="🎉")
+
+# Set up navigation
+pg = st.navigation([main_page, TimeTable, QuizMaker, Leaderboard])
+
+# Run the selected page
+pg.run()
